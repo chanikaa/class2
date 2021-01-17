bef6404a7d4bbd3eea39b48212092a9cc2f9f24a
DESKTOP-JLV7S3S\User <chanika.n@ku.th>
2021-01-17 10:48:59 +0700

    add new tuple folder and new 3 files

---
+++ tuples/acces_tuple_items.py
@@ -0,0 +1,11 @@
+thistuple = ("apple", "banana", "cherry")
+
+print(thistuple[0])  # output: apple
+print(thistuple[1])  # output: banana
+print(thistuple[2])  # output: cherry
+
+print(thistuple[-1])  # output: cherry
+print(thistuple[-2])  # output: banana
+print(thistuple[-3])  # output: apple
+
+print(thistuple[1:])  # output: ['banana', 'cherry']
